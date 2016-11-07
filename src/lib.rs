9a9484635a9c4311d0a89c887d1bc38dec5c8ad7
Chris Dawes <cmsd2@cantab.net>
2016-11-07 21:50:32 +0000

    interval tree clocks structs

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+
+#[derive(Debug, Clone)]
+pub enum IdTree {
+    Leaf {
+        i: bool
+    },
+    Node {
+        left: Box<IdTree>,
+        right: Box<IdTree>
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum EventTree {
+    Leaf {
+        n: u32
+    },
+    Node {
+        n: u32,
+        left: Box<EventTree>,
+        right: Box<EventTree>
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Stamp {
+    i: Box<IdTree>,
+    e: Box<EventTree>
+}
+
+impl IdTree {
+    pub fn zero() -> IdTree {
+        IdTree::Leaf {
+            i: false
+        }
+    }
+
+    pub fn one() -> IdTree {
+        IdTree::Leaf {
+            i: true
+        }
+    }
+
+    pub fn node(left: Box<IdTree>, right: Box<IdTree>) -> IdTree {
+        IdTree::Node {
+            left: left,
+            right: right
+        }
+    }
+}
+
+impl EventTree {
+    pub fn leaf(n: u32) -> EventTree {
+        EventTree::Leaf {
+            n: n
+        }
+    }
+
+    pub fn node(n: u32, left: Box<EventTree>, right: Box<EventTree>) -> EventTree {
+        EventTree::Node {
+            n: n,
+            left: left,
+            right: right
+        }
+    }
+}
+
+impl Stamp {
+    pub fn new(i: Box<IdTree>, e: Box<EventTree>) -> Stamp {
+        Stamp {
+            i: i,
+            e: e
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
